874e6edb2bf155aa130d61d2ebaa9314e16501ca
Christopher Tarry <chrisjtarry@gmail.com>
2023-07-09 15:46:32 -0400

    remove unnecessary

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eux
-gofmt -s -w .
-~/prog/go/bin/goimports -w .
-~/prog/go/bin/easyjson -all ./api/types.go
-go build ./cmd/fifa
-# rm fifa
--- scripts/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eux
-gofmt -s -w .
-~/prog/go/bin/goimports -w .
-~/prog/go/bin/easyjson -all ./api/types.go
-go run ./cmd/fifa
